dcd1dd4fdc69d9b7684d08dfa40a0dd0cc51b55b
philip ka sing leung <philip.leung90@gmail.com>
2017-12-11 04:54:00 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget(){
+  return document.querySelector(".target")
+}
+
+function increaseRankBy(n){
+  var rank = document.querySelectorAll("ul.ranked-list li");
+  for(var i = 0; i<rank.length;i++){
+    var number = parseInt(rank[i].innerHTML);
+    rank[i].innerHTML = (number + n).toString();
+  }
+}
+
+function deepestChild(){
+  return document.querySelector("div#grand-node div div div div")
+}
